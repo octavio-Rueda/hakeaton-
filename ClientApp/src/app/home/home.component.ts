a7c39d454089a989dbce9966dd6d7dfb3253d28d
octavio-Rueda <octavioruedahh@gmail.com>
2020-11-18 02:43:19 -0600

    Co-authored-by: luis manuel diaz sequea <luismanueldiazsequea@gmail.com>

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -5,6 +5,7 @@ import { Gallery } from '../Models/Gallery';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
 
